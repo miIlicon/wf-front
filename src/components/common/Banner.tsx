f1dade8c588a9f77e4df0446fb2cb0cd28280be8
klmhyeonwooo <gentlemonster77@naver.com>
2023-10-05 00:38:19 +0900

    feat : 메인 배너 Swiper 라이브러리 연동 및 재사용 가능한 배너 컴포넌트 생성 (#103)

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -2,103 +2,156 @@
 import { css } from "@emotion/react";
 import React from "react";
 import { ReactComponent as BannerArrow } from "../../images/bannerArrow.svg";
-import { ReactComponent as Rocket } from "../../images/rocket.svg";
+import rocket from "../../images/rocket.svg";
+import lamb from "../../images/banner/lamb.svg";
+import banner1 from "../../images/banner/banner1.png";
+import banner2 from "../../images/banner/banner2.png";
+import banner3 from "../../images/banner/banner3.png";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 export default function Banner() {
   const navigate = useNavigate();
 
-  return (
-    <div
-      css={css`
-        height: 7em;
-        border: solid;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        background-color: black;
-        color: white;
-        letter-spacing: -0.03em;
-      `}
-    >
+  function Container({
+    backgroundColor,
+    backgroundImg,
+    content,
+    router,
+    imgSrc,
+    defaultImgSize,
+  }: any) {
+    return (
       <div
         css={css`
+          height: 7em;
+          border: solid;
           display: flex;
-          column-gap: 1em;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          background: ${backgroundImg ? `url(${backgroundImg})` : ""};
+          background-repeat: no-repeat;
+          background-position: 80% 30%;
+          background-color: ${backgroundColor ? backgroundColor : ""};
+          color: white;
+          letter-spacing: -0.03em;
         `}
       >
         <div
           css={css`
             display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            row-gap: 0.6em;
+            column-gap: 1em;
           `}
         >
-          <span
-            css={css`
-              display: block;
-              font-size: 19.2px;
-              font-family: "Pretendard-ExtraBold";
-
-              @media (max-width: 479px) {
-                font-size: 15px;
-              }
-              @media all and (min-width: 480px) and (max-width: 767px) {
-                font-size: 16px;
-              }
-              @media all and (min-width: 768px) and (max-width: 1099px) {
-                font-size: 17px;
-              }
-              @media all and (min-width: 1100px) {
-                font-size: 18px;
-              }
-            `}
-          >
-            지금 진행중인 축제를 보러가볼까요?
-          </span>
           <div
             css={css`
-              width: 100%;
               display: flex;
-              justify-content: left;
+              flex-direction: column;
+              justify-content: center;
               align-items: center;
-              transition: 0.4s all;
-              cursor: pointer;
-
-              &:hover {
-                opacity: 80%;
-              }
+              row-gap: 0.6em;
             `}
-            onClick={() => navigate("/detail")}
           >
             <span
               css={css`
-                display: flex;
-                align-items: center;
-                justify-content: center;
+                display: block;
+                font-size: 19.2px;
+                font-family: "Pretendard-ExtraBold";
 
                 @media (max-width: 479px) {
-                  font-size: 13px;
-                }
-                @media all and (min-width: 480px) and (max-width: 767px) {
-                  font-size: 14px;
-                }
-                @media all and (min-width: 768px) and (max-width: 1099px) {
                   font-size: 15px;
                 }
-                @media all and (min-width: 1100px) {
+                @media all and (min-width: 480px) and (max-width: 767px) {
                   font-size: 16px;
+                }
+                @media all and (min-width: 768px) and (max-width: 1099px) {
+                  font-size: 17px;
+                }
+                @media all and (min-width: 1100px) {
+                  font-size: 18px;
                 }
               `}
             >
-              자세히 보러가기
+              {content}
             </span>
-            <BannerArrow
+            <div
               css={css`
+                width: 100%;
+                display: flex;
+                justify-content: left;
+                align-items: center;
+                transition: 0.4s all;
+                cursor: pointer;
+
+                &:hover {
+                  opacity: 80%;
+                }
+              `}
+              onClick={() => navigate(`${router}`)}
+            >
+              <span
+                css={css`
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+
+                  @media (max-width: 479px) {
+                    font-size: 13px;
+                  }
+                  @media all and (min-width: 480px) and (max-width: 767px) {
+                    font-size: 14px;
+                  }
+                  @media all and (min-width: 768px) and (max-width: 1099px) {
+                    font-size: 15px;
+                  }
+                  @media all and (min-width: 1100px) {
+                    font-size: 16px;
+                  }
+                `}
+              >
+                자세히 보러가기
+              </span>
+              <BannerArrow
+                css={css`
+                  path {
+                    fill: white;
+                  }
+                  @media (max-width: 479px) {
+                    font-size: 13px;
+                  }
+                  @media all and (min-width: 480px) and (max-width: 767px) {
+                    font-size: 14px;
+                  }
+                  @media all and (min-width: 768px) and (max-width: 1099px) {
+                    font-size: 15px;
+                  }
+                  @media all and (min-width: 1100px) {
+                    font-size: 16px;
+                  }
+                  width: 0.9em;
+                `}
+              />
+            </div>
+          </div>
+          {imgSrc && (
+            <img
+              src={imgSrc}
+              alt="배너 부가 이미지"
+              css={css`
+                width: ${defaultImgSize}em;
+                height: auto;
+
                 @media (max-width: 479px) {
                   font-size: 13px;
                 }
@@ -111,31 +164,57 @@ export default function Banner() {
                 @media all and (min-width: 1100px) {
                   font-size: 16px;
                 }
-                width: 0.9em;
               `}
             />
-          </div>
+          )}
         </div>
-        <Rocket
-          css={css`
-            width: 5em;
-            height: auto;
-
-            @media (max-width: 479px) {
-              font-size: 13px;
-            }
-            @media all and (min-width: 480px) and (max-width: 767px) {
-              font-size: 14px;
-            }
-            @media all and (min-width: 768px) and (max-width: 1099px) {
-              font-size: 15px;
-            }
-            @media all and (min-width: 1100px) {
-              font-size: 16px;
-            }
-          `}
-        />
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Container
+            backgroundImg={banner2}
+            // backgroundColor={`#004F3C`}
+            content={`다시 돌아온 대나무 숲을 이용해보세요`}
+            router={`/detail`}
+            imgSrc={lamb}
+            defaultImgSize={"7"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Container
+            backgroundImg={banner1}
+            // backgroundColor={`#404040`}
+            content={`지금 인기 있는 축제를 만나보세요`}
+            router={`/detail`}
+            // imgSrc={rocket}
+            defaultImgSize={"5"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Container
+            backgroundImg={banner3}
+            content={`밀리콘 팀에 합류하고 싶으신가요?`}
+            router={`/detail`}
+            // imgSrc={lamb}
+            defaultImgSize={"7"}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
