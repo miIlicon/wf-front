76e50032e4b3de63eda4299ebc3e122fa54e2a2b
klmhyeonwooo <gentlemonster77@naver.com>
2023-10-09 01:49:42 +0900

    feat : 프론트엔드 프로덕션 환경으로 API 변경 (#70)

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   // 로컬 방식에서 사용을 할 때는 아래 API를 이용해주세요
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://server.withfestival.site/",
 
   // 프로덕션 환경에서는 아래 API를 사용해주세요
-  // baseURL: "https://api.withfestival.site",
+  // baseURL: "https://server.withfestival.site/",
 });
 
 export default API;
